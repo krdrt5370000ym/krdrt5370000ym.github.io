b56b23971ed1ad8a6bca128edc3fac22759b42a3
krdrt5370000ym <160354554+krdrt5370000ym@users.noreply.github.com>
2025-09-12 23:48:49 +0200

    Update fontchange.js

--- fontchange.js
+++ fontchange.js
@@ -1,4 +1,4 @@
 function changeFontFamily() {
   var fontFamily = document.querySelector(".fontFamily").value;
-  document.querySelector("body").style.fontFamily = fontFamily;
+  document.querySelector("body,h1,h2,h3,h4,h5,h6").style.fontFamily = fontFamily;
 }
