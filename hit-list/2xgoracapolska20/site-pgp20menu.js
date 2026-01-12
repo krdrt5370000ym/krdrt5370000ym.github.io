7c8a7c48311d0551ec7831105ba10384a4a31052
krdrt5370000ym <160354554+krdrt5370000ym@users.noreply.github.com>
2026-01-12 17:41:09 +0100

    Create site-pgp20menu.js

---
+++ hit-list/2xgoracapolska20/site-pgp20menu.js
@@ -0,0 +1,1 @@
+document.write('<div id=\"menusub\"><div id=\"menuranking\"><div class=\"menulist\"><a href=\"https://krdrt5370000ym.github.io/hit-list/2xgoracapolska20/2025\">2025</a></div></div></div>');
