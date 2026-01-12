45f2f840faa739127fdf851e47e7c93eb24a7d27
krdrt5370000ym <160354554+krdrt5370000ym@users.noreply.github.com>
2026-01-12 17:41:55 +0100

    Create site-blbestofmenu.js

---
+++ hit-list/bestlista-bestof/site-blbestofmenu.js
@@ -0,0 +1,1 @@
+document.write('<div id=\"menusub\"><div id=\"menuranking\"><div class=\"menulist\"><a href=\"https://krdrt5370000ym.github.io/hit-list/bestlista-bestof/2025\">2025</a></div></div></div>');
