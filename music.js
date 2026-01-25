c2fc365d467ff51783f60387503a7641a2193680
krdrt5370000ym <160354554+krdrt5370000ym@users.noreply.github.com>
2026-01-25 23:11:08 +0100

    Update music.js

--- music.js
+++ music.js
@@ -1,16 +1,17 @@
-;/*SIMPLE AUDIO PLAYER*/(() => {
-  // https://stackoverflow.com/a/34487069/383904
-  const AUD = new Audio(),
+; /*SIMPLE AUDIO PLAYER*/
+(() => {
+   // https://stackoverflow.com/a/34487069/383904
+   const AUD = new Audio(),
       BTNS = document.querySelectorAll("[data-audio]");
-  
-  function playPause() {
-    const src = this.dataset.audio;
-    if (AUD.src != src) AUD.src = src;
-    AUD[AUD.paused ? "play" : "pause"]();
-    BTNS.forEach(el => el.classList.remove("pause"));
-    this.classList.toggle("pause", !AUD.paused);
-  }
-  
-  AUD.addEventListener("ended", playPause);
-  BTNS.forEach(el => el.addEventListener("click", playPause));
+
+   function playPause() {
+      const src = this.dataset.audio;
+      if (AUD.src != src) AUD.src = src;
+      AUD[AUD.paused ? "play" : "pause"]();
+      BTNS.forEach(el => el.classList.remove("pause"));
+      this.classList.toggle("pause", !AUD.paused);
+   }
+
+   AUD.addEventListener("ended", playPause);
+   BTNS.forEach(el => el.addEventListener("click", playPause));
 })();
