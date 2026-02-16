8711ffbb9f2df74b86f20d429d88e2d10fcac312
krdrt5370000ym <160354554+krdrt5370000ym@users.noreply.github.com>
2026-02-16 23:28:45 +0100

    Update radios.js

--- player/radios.js
+++ player/radios.js
@@ -49,7 +49,7 @@
         element.classList.add('active');
         
         title.innerText = "Teraz grasz: " + station.name;
-        player.src = station.url.replace(/^http:\/\//, "http://");
+        player.src = station.url.replace(/^http:\/\//, "");
         player.play();
         player.style='display:initial;';
         document.getElementById('buttons').style='display:initial;';
