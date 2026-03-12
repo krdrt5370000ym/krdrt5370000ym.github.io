2cd314030413c15487a78f23f83089fced65209e
krdrt5370000ym <160354554+krdrt5370000ym@users.noreply.github.com>
2026-03-12 21:48:10 +0100

    Update site-article.js

--- site-article.js
+++ site-article.js
@@ -104,11 +104,13 @@ async function WPArticle(mainUrl, is_categories = true, is_author = true, is_ima
             if (is_image) {
                 if (post.featured_media !== 0) {
                     if (!cache.images[post.featured_media]) {
-                        const imagesRes = await fetch(`${mainUrl}/wp-json/wp/v2/media/${post.featured_media}`);
-                        const imagesData = await imagesRes.json();
-                        cache.images[post.featured_media] = imagesData.media_details?.sizes?.medium?.source_url || '';
+                        try {
+                            const imagesRes = await fetch(`${mainUrl}/wp-json/wp/v2/media/${post.featured_media}`);
+                            const imagesData = await imagesRes.json();
+                            cache.images[post.featured_media] = imagesData.media_details?.sizes?.medium?.source_url || imagesData.source_url;
+                        } catch (e) { cache.images[post.featured_media] = ''; }
                     }
-                    imageDisplay = `<img src="${cache.images[post.featured_media]}" width="150" height="150">`;
+                    imageDisplay = cache.images[post.featured_media] ? `<img src="${cache.images[post.featured_media]}" width="150" height="150">` : '';
                 }
             }
 
