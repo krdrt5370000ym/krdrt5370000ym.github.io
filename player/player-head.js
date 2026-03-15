0499c5fbfcef65972373882acf61cd95d5387f5a
krdrt5370000ym <160354554+krdrt5370000ym@users.noreply.github.com>
2026-03-15 21:41:20 +0100

    Update player-head.js

--- player/player-head.js
+++ player/player-head.js
@@ -1,1 +1,1 @@
-document.write('<script src=\"https://cdn.jsdelivr.net/npm/hls.js@1.6.15/dist/hls.min.js\"></script>');
+document.write('<script src=\"https://cdn.jsdelivr.net/npm/hls.js@latest\"></script>');
