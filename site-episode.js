e891a8821c04f347bb4b8939afefe1bbf256cdd4
krdrt5370000ym <160354554+krdrt5370000ym@users.noreply.github.com>
2026-03-23 22:53:07 +0100

    Update site-episode.js

--- site-episode.js
+++ site-episode.js
@@ -1,3 +1,4 @@
+let = hls;
 // <div id="episode-list">Ładowanie odcinków...</div>
 function SpreakerPodcast(showId) {
     // Dodanie parametru limit=100 pozwala pobrać więcej odcinków w jednym zapytaniu
@@ -149,16 +150,13 @@ function WPPodcast(categoryId,mainUrl) {
 // WPPodcast(5,"https://radiorsc.pl");
 
 function AgoraPodcast(brandId, seriesId, mainUrl) {
-    // Dodanie parametru limit=100 pozwala pobrać więcej odcinków w jednym zapytaniu
     const apiUrl = 'https://podcasts.radioagora.pl/api/getPodcasts?brand_id=' + brandId + '&limit=100&offset=0&series_id=' + seriesId;
     const container = document.getElementById('episode-list');
 
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => {
-            // Dostęp do tablicy odcinków: response -> items
-            // Bezpieczne pobranie rekordów z głębokiej struktury
-            const episodes = data.records;
+            const episodes = data.records || [];
             
             if (episodes.length === 0) {
                 container.innerHTML = "Brak dostępnych odcinków.";
@@ -169,19 +167,35 @@ function AgoraPodcast(brandId, seriesId, mainUrl) {
                 `<ul class="podcast_list_episode_content">
                     <li class="podcast_list_episode_title">
                         <a href="${mainUrl}/podcast/${episode.podcast_seo_url}/${episode.podcast_id}" target="_blank">${episode.podcast_name}</a> 
+                        <a href="#" onclick="
+                            GetAndPlayAgora(${brandId}, ${episode.podcast_id});
+                            return false;
+                        ">►</a>
                     </li>
                 </ul>`
             ).join('');
 
             container.innerHTML = htmlContent;
         })
-        .catch(error => {
-            console.error("Błąd API:", error);
-            container.innerHTML = "Błąd połączenia z API Agory.";
+        .catch(err => container.innerHTML = "Błąd API.");
+}
+
+// Funkcja pomocnicza pobierająca konkretny strumień przed odtworzeniem
+function GetAndPlayAgora(brandId, podcastId) {
+    const detailUrl = `https://podcasts.radioagora.pl/api/universalApigetPodcastAll?brand_id=${brandId}&podcast_id=${podcastId}`;
+    
+    fetch(detailUrl)
+        .then(res => res.json())
+        .then(data => {
+            // Zakładając, że URL strumienia jest w data.podcast_info.player.stream lub podobnej strukturze
+            const streamUrl = data.url;
+            if (streamUrl) {
+                AudioPlayerEpisode(streamUrl);
+            } else {
+                alert("Nie znaleziono źródła dźwięku.");
+            }
         });
 }
-// Poprawna kolejność: brandId (1) [Rock Radio, itp.], seriesId (176)
-// AgoraPodcast(1,176,"https://radio.rockradio.pl");
 
 function WPPodcastRK(SearchId) {
     // WordPress API zwraca tablicę postów bezpośrednio
