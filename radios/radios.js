9f55fe6209f16b2793f92184ecee2cec5f76952d
krdrt5370000ym <160354554+krdrt5370000ym@users.noreply.github.com>
2026-04-15 11:25:13 +0200

    Update radios.js

--- radios/radios.js
+++ radios/radios.js
@@ -454,7 +454,7 @@ function renderPrograms() {
         <div>
             ${programUrl}
             <div class="program_list_host">${p.only_the_schedule_hosts === true ? '' : p.host}</div>
-            <div class="program_list_host">${p.onair}</div>
+            <div class="program_list_onair">${p.onair}</div>
         </div>
       `;
 
