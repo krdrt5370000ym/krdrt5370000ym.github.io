d852d7093fc28f7e9444a49c4ac2d2d1b47f6eb5
krdrt5370000ym <160354554+krdrt5370000ym@users.noreply.github.com>
2026-04-28 23:11:28 +0200

    Update site-sidebar.js

--- site-sidebar.js
+++ site-sidebar.js
@@ -87,3 +87,10 @@ function hackBodyFont(fontName = 'Roboto', option = 1, customWeights = '') {
 // Przykłady użycia:
 // hackBodyFont('Montserrat', 0); // Pełny zakres wag + kursywa
 // hackBodyFont('Open Sans', 3, '300;800'); // Własne wagi
+
+// Jak tego używać w Chrome?
+// Otwórz dowolną stronę WWW (np. Google.pl, Wikipedia).
+// Skasuj wszystko z paska adresu.
+// Wpisz ręcznie słowo javascript: (Chrome często usuwa ten człon przy wklejaniu ze schowka dla bezpieczeństwa).
+// Wklej resztę kodu zaraz po dwukropku i naciśnij Enter.
+// javascript:(function(f,o,w){const m={0:'ital,wght@0,100..900;1,100..900',1:'wght@400;600;700',2:'wght@400;700',3:'wght@'+w};const s=m[o];const u=`https://fonts.googleapis.com/css2?family=${f.replace(/ /g,'+')}:${s}&display=swap`;let l=document.getElementById('gf-h');if(!l){l=document.createElement('link');l.id='gf-h';l.rel='stylesheet';document.head.appendChild(l)}l.href=u;let t=document.getElementById('bf-s');if(!t){t=document.createElement('style');t.id='bf-s';document.head.appendChild(t)}t.textContent=`body,body *{font-family:'${f}',sans-serif !important}`;})('Roboto',1,'');
